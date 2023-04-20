512d8474633b6ea29aa23ebc03fd13a1f36c7fea
mkundan1489 <114367938+mkundan1489@users.noreply.github.com>
2023-04-20 01:28:52 -0400

    Add files via upload

---
+++ test_with_compound_score.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Initialize the cash and stock holdings
+cash_held = 10000
+stocks_held = 25
+initial_portfolio_value = 10000
+remaining_cash = 10000 - test_data['Open'][0] * stocks_held
+
+# Initialize portfolio value, cash, and stocks held
+portfolio_value = initial_portfolio_value
+cash_held = remaining_cash
+stocks_held = 25
+
+portfolio_values = []
+
+# Iterate through each day in the test dataset
+for i in range(len(test_data)):
+    # Define the state for the current day
+    state = np.array([test_data['Open'][i], test_data['5-day MA'][i], portfolio_value, cash_held, stocks_held, test_data['compound'][i-1]])
+
+    # Take an action using the trained DQN model
+    Q_values = model2.predict(np.array([state]))
+    action = np.argmax(Q_values)
+
+    # Calculate the reward for the action taken
+    price_change_percentage = ((test_data['Open'][i] - test_data['5-day MA'][i]) / test_data['5-day MA'][i]) * 100
+    reward = price_change_percentage
+
+    # Update the cash and stock holdings based on the action taken
+    if action == 0:
+        stocks_bought = cash_held // test_data['Open'][i]
+        cash_held -= stocks_bought * test_data['Open'][i]
+        stocks_held += stocks_bought
+        print("buy")
+    elif action == 1:
+        cash_gained = stocks_held * test_data['Open'][i]
+        cash_held += cash_gained
+        stocks_held = 0
+        print("sell")
+    elif action == 2:
+        print("hold")
+
+    # Update the portfolio value with the current cash and stock holdings
+    portfolio_value = cash_held + stocks_held * test_data['Open'][i]
+    portfolio_values.append(portfolio_value)
+#print portfolio value
+print(portfolio_value)
+portfolio_values2 = portfolio_values
+# Plot the portfolio value over time
+plt.plot(range(len(test_data)), portfolio_values)
+plt.xlabel('Day')
+plt.ylabel('Portfolio Value')
+plt.savefig('portfolio_values_2.png')
+plt.show()
+
---
+++ train_with_compound_score.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+from tensorflow import keras
+
+data = pd.read_csv("aaplCombined.csv")
+# Calculate the 5-day moving average
+data['5-day MA'] = data['Close'].rolling(window=5).mean()
+data = data.dropna()
+# Split the data into training and test sets
+train_data = data[:1513]
+test_data = data[1513:]
+
+
+# Define hyperparameters
+batch_size = 50
+gamma = 0.95
+memory_size = 10000
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_decay = 0.995
+learning_rate = 0.001
+update_target_network = 100
+
+# Define the deep neural network model
+model2 = keras.models.Sequential([
+    keras.layers.Dense(64, input_shape=(6,), activation='relu'),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(3, activation='linear')
+])
+model2.compile(optimizer=keras.optimizers.Adam(learning_rate), loss='mse')
+
+# Define the target network model
+target_model2 = keras.models.clone_model(model2)
+target_model2.set_weights(model2.get_weights())
+
+# Define the experience replay function
+def experience_replay(agent_memory):
+    # Sample a batch from agent memory
+    batch_indices = np.random.choice(len(agent_memory), batch_size, replace=False)
+    batch = [agent_memory[i] for i in batch_indices]
+
+    # Unpack the batch into states, actions, rewards, and next states
+    states = np.array([b[0] for b in batch])
+    actions = np.array([b[1] for b in batch])
+    rewards = np.array([b[2] for b in batch])
+    next_states = np.array([b[3] for b in batch])
+    done_flags = np.array([b[4] for b in batch])
+
+    # Predict the Q values for the current states and the next states
+    current_Q = model2.predict(states)
+    next_Q = target_model2.predict(next_states)
+
+    # Calculate the target Q values
+    target_Q = np.zeros(batch_size)
+    for i in range(batch_size):
+        if done_flags[i]:
+            target_Q[i] = rewards[i]
+        else:
+            target_Q[i] = rewards[i] + gamma * np.max(next_Q[i])
+
+    # Update the Q values for the current states and actions
+    current_Q[np.arange(batch_size), actions.astype(int)] = target_Q
+
+    # Train the deep neural network with the updated Q values
+    model2.fit(states, current_Q, epochs=1, verbose=0)
+
+# Load the stock market data
+data = train_data
+
+# Initialize the agent memory
+agent_memory = []
+
+# Initialize the cash and stock holdings
+cash_held = 10000
+stocks_held = 25
+initial_portfolio_value = 10000
+remaining_cash = 10000 - data['Open'][0] * stocks_held
+
+# Initialize exploration rate
+exploration_rate = epsilon
+
+# Define the number of episodes
+num_episodes = 50
+
+# Iterate through each episode
+for episode in range(num_episodes):
+    # Initialize portfolio value, cash, and stocks held
+    portfolio_value = initial_portfolio_value
+    cash_held = remaining_cash
+    stocks_held = 25
+    print('number of episode', episode)
+    # Iterate through each day in the dataset
+    for i in range(len(data)):
+        # Define the state for the current day
+        state = np.array([data['Open'][i], data['5-day MA'][i], portfolio_value, cash_held, stocks_held, data['compound'][i-1]])
+
+        # Take an action using an epsilon-greedy policy
+        if np.random.rand() < exploration_rate:
+            #action = random.choice(['Buy', 'Sell', 'Hold'])
+            action = random.randrange(3)
+        else:
+            Q_values = model2.predict(np.array([state]))
+            action = np.argmax(Q_values)
+
+        # Calculate the reward for the action taken
+        price_change_percentage = ((data['Open'][i] - data['5-day MA'][i]) / data['5-day MA'][i]) * 100
+        reward = price_change_percentage
+
+        # Calculate the reward for the action taken
+        if action == 0:
+            stocks_bought = cash_held // data['Open'][i]
+            cash_held -= stocks_bought * data['Open'][i]
+            stocks_held += stocks_bought
+        elif action == 1:
+            cash_gained = stocks_held * data['Open'][i]
+            cash_held += cash_gained
+            stocks_held = 0
+        elif action == 2:
+            pass
+
+        # Update the portfolio value with the current cash and stock holdings
+        portfolio_value = cash_held + stocks_held * data['Open'][i]
+
+        # Update the agent memory with the current state, action, reward, next state, and done flag
+        next_state = np.array([data['Open'][i], data['5-day MA'][i], portfolio_value, cash_held, stocks_held, data['compound'][i-1]])
+        done_flag = (i == len(data) - 1)
+        agent_memory.append((state, action, reward, next_state, done_flag))
+
+        # Update the exploration rate
+        exploration_rate = max(epsilon_min, exploration_rate * epsilon_decay)
+
+        # Update the target network every 'update_target_network' steps
+        if len(agent_memory) % update_target_network == 0:
+            target_model2.set_weights(model2.get_weights())
+
+        # Perform experience replay every step after memory_size is reached
+        if len(agent_memory) >= memory_size:
+            experience_replay(agent_memory)
+
+model2.save('/mnt/home/mahaseth/CMSEProj/model2.h5')
